c0a07fb185eec2b31b46475c9c46a3aff52afe45
tanishk15sharma <tanishk1582002@gmail.com>
2022-01-11 02:30:25 +0530

    redux-ToolKIT setup

---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  movies: [],
+};
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducer: {},
+});
