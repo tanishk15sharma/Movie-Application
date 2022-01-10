6cbcac36469664e1b91ed2da16efb1fdce6e91f0
tanishk15sharma <tanishk1582002@gmail.com>
2022-01-11 02:15:23 +0530

    oMDb API call

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/movielisting";
+import movieApi from "../../apis/movieApi";
+import { APIkey } from "../../apis/movieApiKey";
 
 const Home = () => {
-  return <div>homee</div>;
+  // let apis = `http://www.omdbapi.com/?apikey=7baec597&s=Harry&type=movie`;
+
+  useEffect(() => {
+    const text = "harry";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIkey}&s=${text}&type=movie`)
+        .catch((err) => {
+          console.log(`error:${err} `);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
