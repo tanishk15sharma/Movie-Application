c1ddcd4438f73462b7afc076dc8cf747ed81b9af
tanishk15sharma <tanishk1582002@gmail.com>
2022-01-11 17:09:42 +0530

    sending movie-Results to movieLisiting using useSelector

--- src/components/MovieListing/movielisting.js
+++ src/components/MovieListing/movielisting.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { getAllMovies } from "../../features/movies/movieSlice";
 import { useSelector } from "react-redux";
+import MovieCard from "../MovieCard/moviecard";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
-  console.log(movies);
+  console.log(movies); //movies data is coming from the store
 
-  return <div>movie listinggg</div>;
+  let renderMovies = "";
+
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => {
+        return <MovieCard key={index} data={movie} />;
+      })
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
+
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>MOVIES</h2>
+        <div className="movie-container"> {renderMovies} </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
