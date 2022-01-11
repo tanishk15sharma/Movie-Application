02a68061fca5f9ad6e727a1a994ad47b8a18a795
tanishk15sharma <tanishk1582002@gmail.com>
2022-01-11 19:14:54 +0530

    Movie-Card Updated

--- src/components/MovieCard/moviecard.js
+++ src/components/MovieCard/moviecard.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const MovieCard = () => {
-  return <div>moviecard</div>;
+const MovieCard = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.Title}</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
