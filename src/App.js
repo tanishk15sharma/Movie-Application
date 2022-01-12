45c1bb698b78a843bd7007590e4547349e74f8fe
tanishk15sharma <tanishk1582002@gmail.com>
2022-01-12 19:48:28 +0530

    feat : loading msg

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import "./App.scss";
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header.js";
 import Home from "./components/Home/home";
-import MovieCard from "./components/MovieCard/moviecard";
 import MovieDetail from "./components/MovieDetail/moviedetail";
-import MovieListing from "./components/MovieListing/movielisting";
 import PageNotFound from "./components/PageNotFound/pagenotfound";
 
 function App() {
--- src/components/MovieDetail/moviedetail.js
+++ src/components/MovieDetail/moviedetail.js
@@ -13,7 +13,7 @@ const MovieDetail = () => {
   const dispatch = useDispatch();
 
   const idData = useSelector(getSelectedMovieOrSeries); //getting  IDdata from store
-  console.log(idData);
+  // console.log(idData);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrSeries(imdbID)); //sending specific id to slice
 
@@ -25,53 +25,60 @@ const MovieDetail = () => {
 
   return (
     <div className="movie-section">
-      <div className="section-left">
-        <div className="movie-title">{idData.Title} </div>
-        <div className="movie-rating">
-          <span>
-            IMBD Rating <i className="fa fa-star"></i> : {idData.imdbRating}
-          </span>
-          <span>
-            IMBD Votes <i className="fa fa-thumbs-up"></i> : {idData.imdbVotes}
-          </span>
-          <span>
-            Run Time <i className="fa fa-film"></i> : {idData.Runtime}
-          </span>
-          <span>
-            Year <i className="fa fa-calendar"></i> : {idData.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{idData.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director </span>
-            <span>{idData.Director}</span>
+      {Object.keys(idData).length === 0 ? (
+        <div>...loading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{idData.Title} </div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i> : {idData.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i> :{" "}
+                {idData.imdbVotes}
+              </span>
+              <span>
+                Run Time <i className="fa fa-film"></i> : {idData.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i> : {idData.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{idData.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director </span>
+                <span>{idData.Director}</span>
+              </div>
+              <div>
+                <span>Released </span>
+                <span>{idData.Released}</span>
+              </div>
+              <div>
+                <span>Stars </span>
+                <span>{idData.Actors}</span>
+              </div>
+              <div>
+                <span>Generes </span>
+                <span>{idData.Genre}</span>
+              </div>
+              <div>
+                <span>Languages </span>
+                <span>{idData.Language}</span>
+              </div>
+              <div>
+                <span>Awards </span>
+                <span>{idData.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Released </span>
-            <span>{idData.Released}</span>
+          <div className="section-right">
+            <img src={idData.Poster} alt={idData.imdbID} />
           </div>
-          <div>
-            <span>Stars </span>
-            <span>{idData.Actors}</span>
-          </div>
-          <div>
-            <span>Generes </span>
-            <span>{idData.Genre}</span>
-          </div>
-          <div>
-            <span>Languages </span>
-            <span>{idData.Language}</span>
-          </div>
-          <div>
-            <span>Awards </span>
-            <span>{idData.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={idData.Poster} alt={idData.imdbID} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
