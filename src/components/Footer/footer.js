e767887e23f2d1fa9bbfb1508492a185b6e9a979
tanishk15sharma <tanishk1582002@gmail.com>
2022-02-25 17:08:23 +0530

    fix footer link

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,16 +5,13 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footerlink">
-        <a href="https://github.com/tanishk15sharma" target="_blank">
+        <a href="https://github.com/tanishk15sharma">
           <i class="fab fa-github-square github-icon   link-icon  "></i>
         </a>
-        <a href="https://twitter.com/_tanishksharma" target="_blank">
+        <a href="https://twitter.com/_tanishksharma">
           <i class="fab fa-twitter-square twitter-icon  link-icon "></i>
         </a>
-        <a
-          href="https://www.linkedin.com/in/tanishk-sharma-98225a217/"
-          target="_blank"
-        >
+        <a href="https://www.linkedin.com/in/tanishk-sharma-98225a217/">
           <i class="fab fa-linkedin link-icon  link-icon "></i>
         </a>
       </div>
